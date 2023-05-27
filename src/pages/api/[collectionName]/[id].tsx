e958d815b8e164f20a4d132cf5f97e12c0a07dc5
Bruh <timofejbakurov@gmail.com>
2023-05-27 19:43:37 +0700

    Изменена точка API

--- src/pages/api/[collectionName]/[id].tsx
+++ src/pages/api/[collectionName]/[id].tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export default async function handler(req, res){
-    if(!['classes', 'races', 'backgrounds'].includes(req.query.collectionName)){
-        res.status(404).json({error: "This collection does not exist"});
+    if(req.query.id !== undefined){
+        const item = await axios.get(`http://127.0.0.1:8090/api/collections/${req.query.collectionName}/records/${req.query.id}`);
+        res.status(200).json(item);
     }
-    const item = await axios.get(`http://locahost:8090/api/${req.query.collectionName}/${req.query.id}`);
 
+    const item = await axios.get(`http://127.0.0.1:8090/api/collections/${req.query.collectionName}/records`);
     res.status(200).json(item);
 }
