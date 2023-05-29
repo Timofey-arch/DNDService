65bba80a80675673f10f9f39142e9352330a321b
SerLap-ctrl <s.laptev2017@yandex.ru>
2023-05-29 12:36:18 +0700

    Добавлен вывод картинок в справочнике

--- components/Desktop/Handbook/HandbookMainPage.tsx
+++ components/Desktop/Handbook/HandbookMainPage.tsx
@@ -1,8 +1,11 @@
 import Logo from '../../../public/images/Logo2.png';
 import Image from "next/image";
 import Link from "next/link";
+import PocketBase from 'pocketbase';
+import {getUrl} from "@next/font/dist/google/utils";
 
 export default function HandbookMainPage(props){
+
     return(
         <div className="handbook_main_container">
             <section className="info_section">
@@ -12,7 +15,7 @@ export default function HandbookMainPage(props){
                         return (<Link 
                                     className="item" href={`/handbook/classes/${p_class.name}?id=${p_class.id}`}
                                 >
-                                    <Image className="logo" alt="Icon" src={Logo}></Image>
+                                <img className={"logo"} src={"http://127.0.0.1:8090/api/files/classes/"+p_class.id+"/" + p_class.field}></img>
                                     {p_class.name}
                                 </Link>
                         )
@@ -28,7 +31,7 @@ export default function HandbookMainPage(props){
                         return (<Link 
                                     className="item" href={`/handbook/races/${race.name}?id=${race.id}`}
                                 >
-                                    <Image className="logo" alt="Icon" src={Logo}></Image>
+                                <img className={"logo"} src={"http://127.0.0.1:8090/api/files/races/"+race.id+"/" + race.field}></img>
                                     {race.name}
                                 </Link>
                         )
