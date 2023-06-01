42be9a6cbcd3f3a585b29c0a2cd1852e8b2a8035
Bruh <timofejbakurov@gmail.com>
2023-06-01 14:48:14 +0700

    Добавлены изображения к происхождениям

--- src/pages/handbook/backgrounds/BackgroundInfoContainer.tsx
+++ src/pages/handbook/backgrounds/BackgroundInfoContainer.tsx
@@ -2,7 +2,7 @@ export default function BackgroundInfoContainer({ background }){
     return(
         <div className="background_info_container">
             <h1>{background.name}</h1>
-            
+            <img src={"http://127.0.0.1:8090/api/files/backgrounds/"+ background.id+"/" + background.field}></img>
             <div><b>Описание: </b> {background.description}</div>
             <div><b>Способность: </b> {background.ability}</div>
         </div>
