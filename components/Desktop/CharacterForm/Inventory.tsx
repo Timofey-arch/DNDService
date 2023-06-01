7888fb35a26193bf7c55639920bc3d2e30eff477
Bruh <timofejbakurov@gmail.com>
2023-06-01 16:23:31 +0700

    Поправлены стили, отвечающие за инвентарь

--- components/Desktop/CharacterForm/Inventory.tsx
+++ components/Desktop/CharacterForm/Inventory.tsx
@@ -6,8 +6,11 @@ export default function Inventory(props: any) {
     const items = props.userData.backgrounds.find((elem: any) => elem.name == props.userData.userBackground).equipment.getEquipment
 
     return (
-
-        items?.map((elem, index) => <div key={index} className="savethrow">{elem}</div>)
-
+        <>
+            <></>
+            <div className="inventory">
+                {items?.map((elem, index) => <div key={index} className="item">{elem}</div>)}
+            </div>
+        </>
     );
 }
